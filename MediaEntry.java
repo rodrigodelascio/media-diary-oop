b4d0c7601b4d5e7629cd8da33025e59423854c73
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2025-06-25 11:52:20 +0100

    Add MediaEntry abstract class implementing Summarisable interface

---
+++ MediaEntry.java
@@ -0,0 +1,33 @@
+public abstract class MediaEntry implements Summarisable {
+    protected int entryId;
+    protected String title;
+    protected String platform;
+    protected String purpose;
+    protected String day;
+    protected int duration; // in minutes
+    protected String emotionalResponse;
+
+    public MediaEntry(int entryId, String title, String platform, String purpose, String day, int duration, String emotionalResponse) {
+        this.entryId = entryId;
+        this.title = title;
+        this.platform = platform;
+        this.purpose = purpose;
+        this.day = day;
+        this.duration = duration;
+        this.emotionalResponse = emotionalResponse;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public abstract String getSummary();
+}
